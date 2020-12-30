a0dd8ab6bd15fbe5a86075c476a6b143e0e0d08c
fuho <ondrej.dolejsi@gmail.com>
2020-12-30 23:34:26 +0100

    compose upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.4.21"
-    id("org.jetbrains.compose") version "0.3.0-build136"
+    id("org.jetbrains.compose") version "0.3.0-build138"
 }
 
 group = "org.fuho.sheefra"
